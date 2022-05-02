fce851c5036096b21338fe5d11d23728a42f34e2
Mihai <M.Lefter@lumc.nl>
2022-05-02 17:01:19 +0200

    Fix description to model error

--- mutalyzer_api/endpoints/description_to_model.py
+++ mutalyzer_api/endpoints/description_to_model.py
@@ -1,8 +1,7 @@
 from flask_restx import Namespace, Resource
+from mutalyzer.errors import syntax_uc, syntax_ueof
 from mutalyzer_hgvs_parser import to_model
 from mutalyzer_hgvs_parser.exceptions import UnexpectedCharacter, UnexpectedEnd
-
-import mutalyzer.errors as errors
 
 from .common import errors
 
@@ -17,7 +16,7 @@ class DescriptionToModel(Resource):
         try:
             model = to_model(description)
         except UnexpectedCharacter as e:
-            return {"errors": [errors.syntax_uc(e)]}
+            return {"errors": [syntax_uc(e)]}
         except UnexpectedEnd as e:
-            return {"errors": [errors.syntax_ueof(e)]}
+            return {"errors": [syntax_ueof(e)]}
         return model
